a43b51f0e3194b8e197c9cfdba981e555056c750
bilalseen <bilalsen2010@gmail.com>
2023-03-24 21:23:02 +0300

    post_data.json added

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import {
   TouchableOpacity,
   StyleSheet,
   ScrollView,
+  FlatList,
 } from "react-native";
 import Post from "./components/Post";
+import post_data from "./post_data.json";
 
 function App() {
   return (
@@ -16,41 +18,11 @@ function App() {
         <Text style={styles.header_text}>POST-APP</Text>
       </View>
       <ScrollView style={styles.scrollView}>
-        <Post
-          nickname="yilmaz"
-          text="Halı çalan adamdan herşey beklenir."
-          date="26 minutes ago"
-        />
-        <Post
-          nickname="mandosi_kadir"
-          text="Benim adım Kadir. Dükkanın adı Esat fotoğrafçılık."
-          date="41 minutes ago"
-        />
-        <Post
-          nickname="boyga_hakan"
-          text="Sizden tek bir şey istedim, tek bir şey ya."
-          date="57 minutes ago"
-        />
-        <Post
-          nickname="yilmaz"
-          text="İlkkan bizim senle sırt sırta verip de s*kemeyeceğimiz insan yok, ben buna inanıyorum kardeşim.."
-          date="1 hour ago"
-        />
-        <Post
-          nickname="ilkkan"
-          text="Avcı ne kadar hile bilirse, ayı da o kadar yol bilir."
-          date="1 hour ago"
-        />
-        <Post
-          nickname="yilmaz"
-          text="Ben yanlış geldiğimi anladım da siz çok yanlış gözüküyorsunuz. Buraya ‘Mandosi’ yazıp, kendine Esat diyip, Kadir ismiyle olmuyor."
-          date="1 hour ago"
-        />
-        <Post nickname="ilkkan" text="SMS geldi mi lan?" date="2 hour ago" />
-        <Post
-          nickname="yilmaz"
-          text="Ne olduğu önemli değil senin ne anladığın önemli."
-          date="5 hour ago"
+        <FlatList
+          data={post_data}
+          renderItem={({item}) => (
+            <Post nickname={item.nickname} text={item.text} date={item.date} />
+          )}
         />
       </ScrollView>
     </SafeAreaView>
